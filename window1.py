bc6ba219bc5d70dcd0b0578e9a2e183272d9905d
Aloysius Butac <39267018+AloysiusButac@users.noreply.github.com>
2023-02-07 16:37:09 +0800

    First window fine tuning

--- window1.py
+++ window1.py
@@ -3,28 +3,30 @@ from tkinter import ttk
 
 button_font = ("Arial", 14)
 
-class Window1(tk.Tk):
-    def __init__(self, parent, width = 800, height = 600):
+class Window1():
+    def __init__(self, parent, title = "Window 1", width = 800, height = 600):
         root = parent
         window_w = width
         window_h = height
-        window_title = 'Window 1'
         pos_x = (root.winfo_screenwidth() // 2) - (window_w // 2)
-        pos_y = (root.winfo_screenheight() // 2) - (window_h // 2)
+        pos_y = (root.winfo_screenheight() // 2) - (window_h // 2) - 50
         root.geometry('{}x{}+{}+{}'.format(window_w, window_h, pos_x, pos_y))
-        root.title(window_title)
+        root.title(title)
         root.resizable(0, 0)
 
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=7)
-        root.rowconfigure(1, weight=7)
-        root.rowconfigure(2, weight=2, minsize=80)
 
         # Main Widow Frames
-        display_frame = tk.Frame(root)
-        ui_frame = tk.Frame(root)
-        # display_frame = tk.Frame(root, bg="green")
-        # ui_frame = tk.Frame(root, bg="blue")
+        container_frame = tk.Frame(root)
+        display_frame = tk.Frame(container_frame)
+        ui_frame = tk.Frame(container_frame)
+        # container_frame = tk.Frame(root, bg="cyan")
+        # display_frame = tk.Frame(container_frame, bg="green")
+        # ui_frame = tk.Frame(container_frame, bg="blue")
+
+        container_frame.columnconfigure(0, weight=1)
+        container_frame.rowconfigure(0, weight=7)
+        container_frame.rowconfigure(1, weight=7)
+        container_frame.rowconfigure(2, weight=2, minsize=50)
 
         # Display Frame setup
         display_frame.columnconfigure(0, weight=1)
@@ -52,7 +54,8 @@ class Window1(tk.Tk):
         btn3.pack(padx=2, pady=2, ipadx=20, side="right")
 
         #  Window Frames Deployment
-        display_frame.grid(column=0, row=0, rowspan=2, padx=40, pady=5, sticky="news")
-        ui_frame.grid(column=0, row=2, padx=40, pady=5, sticky="news")
+        container_frame.pack(padx=10, pady=10, fill="both")
+        display_frame.grid(column=0, row=0, rowspan=2, padx=40, pady=2, sticky="news")
+        ui_frame.grid(column=0, row=2, padx=40, pady=2, sticky="news")
 
         root.mainloop()
