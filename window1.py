b97dfeefdaea1469e909324262b376829efadfd8
Aloysius Butac <aloysiusbutac@gmail.com>
2023-02-12 14:24:14 +0800

    Window1 redesign, Added image within canvases

--- window1.py
+++ window1.py
@@ -1,30 +1,30 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter import Button
+# from tkinter import ttk
+# from tkinter import Button
+from PIL import Image,ImageTk
 
 class Window1():
     button_font = ("Arial", 14)
 
-    def __init__(self, parent, title = "Window 1", width = 800, height = 600):
-        root = parent
+    def __init__(self, parent, title = "Window 1", width = 1000, height = 600):
+        self.root = parent
         window_w = width
         window_h = height
         if isinstance(parent, tk.Tk):
-            pos_x = (root.winfo_screenwidth() // 2) - (window_w // 2)
-            pos_y = (root.winfo_screenheight() // 2) - (window_h // 2) - 50
-            root.geometry('{}x{}+{}+{}'.format(window_w, window_h, pos_x, pos_y))
-            root.title(title)
-            root.resizable(0, 0)
+            pos_x = (self.root.winfo_screenwidth() // 2) - (window_w // 2)
+            pos_y = (self.root.winfo_screenheight() // 2) - (window_h // 2) - 50
+            self.root.geometry('{}x{}+{}+{}'.format(window_w, window_h, pos_x, pos_y))
+            self.root.title(title)
+            self.root.resizable(0, 0)
 
         # Main Widow Frames
-        container_frame = tk.Frame(root)
+        container_frame = tk.Frame(self.root)
         display_frame = tk.Frame(container_frame)
         ui_frame = tk.Frame(container_frame)
 
-        container_frame.columnconfigure(0, weight=1)
-        container_frame.rowconfigure(0, weight=7)
-        container_frame.rowconfigure(1, weight=7)
-        container_frame.rowconfigure(2, weight=2, minsize=50)
+        container_frame.columnconfigure(0, weight=3)
+        container_frame.columnconfigure(1, weight=1, minsize=300)
+        container_frame.rowconfigure(0, weight=1)
 
         # Display Frame setup
         display_frame.columnconfigure(0, weight=1)
@@ -32,36 +32,45 @@ class Window1():
         display_frame.rowconfigure(0, weight=1)
         display_frame.rowconfigure(1, weight=1)
 
+        ## Create Canvas grid
         canv1 = tk.Canvas(display_frame, bd=0, relief="solid", highlightbackground="#aaa", highlightthickness=2)
         canv2 = tk.Canvas(display_frame, bd=0, relief="solid", highlightbackground="#aaa", highlightthickness=2)
         canv3 = tk.Canvas(display_frame, bd=0, relief="solid", highlightbackground="#aaa", highlightthickness=2)
         canv4 = tk.Canvas(display_frame, bd=0, relief="solid", highlightbackground="#aaa", highlightthickness=2)
-
-        canv1.grid(column=0, row=0, pady=2, padx=2, stick="nw")
-        canv2.grid(column=1, row=0, pady=2, padx=2, stick="ne")
-        canv3.grid(column=0, row=1, pady=2, padx=2, stick="sw")
-        canv4.grid(column=1, row=1, pady=2, padx=2, stick="se")
+        canv1.grid(column=0, row=0, pady=2, padx=2, stick="news")
+        canv2.grid(column=1, row=0, pady=2, padx=2, stick="news")
+        canv3.grid(column=0, row=1, pady=2, padx=2, stick="news")
+        canv4.grid(column=1, row=1, pady=2, padx=2, stick="news")
+        ## Load image on each canvas
+        self.img1 = ImageTk.PhotoImage(Image.open("Koala.jpg").resize([500, 500], Image.BILINEAR))
+        self.img2 = ImageTk.PhotoImage(Image.open("Koala.jpg").resize([500, 500], Image.BILINEAR))
+        self.img3 = ImageTk.PhotoImage(Image.open("Koala.jpg").resize([500, 500], Image.BILINEAR))
+        self.img4 = ImageTk.PhotoImage(Image.open("Koala.jpg").resize([500, 500], Image.BILINEAR))
+        ## Add image on each canvas
+        canv1.create_image(0, 0, anchor=tk.NW, image=self.img2)
+        canv2.create_image(0, 0, anchor=tk.NW, image=self.img2)
+        canv3.create_image(0, 0, anchor=tk.NW, image=self.img3)
+        canv4.create_image(0, 0, anchor=tk.NW, image=self.img4)
 
         # UI Frame setup
-        self.btn1 = tk.Button(ui_frame, text="Button", font=self.button_font, bd=1, relief="solid", foreground="#333") 
-        self.btn2 = tk.Button(ui_frame, text="Button", font=self.button_font, bd=1, relief="solid", foreground="#333")
-        self.btn3 = tk.Button(ui_frame, text="Button", font=self.button_font, bd=1, relief="solid", foreground="#333")
+        self.btn1 = tk.Button(ui_frame, text="NOTIFICATION", font=self.button_font, bd=1, relief="solid", foreground="#333", command=self.ShowNotification) 
+        self.btn2 = tk.Button(ui_frame, text="HISTORY", font=self.button_font, bd=1, relief="solid", foreground="#333", command=self.ShowHistory)
+        self.btn3 = tk.Button(ui_frame, text="MENU", font=self.button_font, bd=1, relief="solid", foreground="#333", command=self.ShowMenu)
 
-        self.btn1.pack(padx=2, pady=2, ipadx=20, side="left")
-        self.btn2.pack(padx=2, pady=2, ipadx=20, side="left")
-        self.btn3.pack(padx=2, pady=2, ipadx=20, side="right")
+        self.btn1.pack(padx=2, pady=2, ipadx=20, side="top", fill=tk.X)
+        self.btn2.pack(padx=2, pady=2, ipadx=20, side="top", fill=tk.X)
+        self.btn3.pack(padx=2, pady=2, ipadx=20, anchor="e", side="bottom")
 
         #  Window Frames Deployment
-        container_frame.pack(padx=10, pady=10, fill="both")
-        display_frame.grid(column=0, row=0, rowspan=2, padx=40, pady=2, sticky="news")
-        ui_frame.grid(column=0, row=2, padx=40, pady=2, sticky="news")
+        container_frame.pack(padx=10, pady=10, fill="both", expand=True)
+        display_frame.grid(column=0, row=0, padx=20, pady=20, sticky="nw")
+        ui_frame.grid(column=1, row=0, padx=10, pady=30, sticky="news")
 
-        root.mainloop()
-    
-    def setCommands():
+    def show(self):
+        self.root.mainloop()
+    def ShowNotification(self):
         pass
-
-    def setButtonCommand(self, button, action):
-        button.configure(command=action)
-    def setButtonFont(self, font):
-        self.button_font = font
+    def ShowHistory(self):
+        pass
+    def ShowMenu(self):
+        pass
