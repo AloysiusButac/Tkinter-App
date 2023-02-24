639cae14f81e68664b6b1f1cfd4e5c0d21d669a1
Aloysius Butac <aloysiusbutac@gmail.com>
2023-02-24 15:45:52 +0800

    Implemented History frame on window1

---
+++ HistoryWindow.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+from PIL import Image,ImageTk
+from VideoCapture import *
+from tkinter import Toplevel
+import cv2
+
+class HistoryWindow:
+    def __init__(self, parent):
+        self.main_container = tk.Frame(parent)
+
+        # =============== Refresh ================
+        btn_container = tk.Frame(self.main_container)
+        btn_container.columnconfigure(0, weight=2)
+        btn_container.columnconfigure(1, weight=2)
+        btn_container.columnconfigure(2, weight=2)
+        btn_container.columnconfigure(3, weight=1)
+        btn_refresh = tk.Button(btn_container, bg="#345", fg="#fff", text="Refresh")
+        btn_refresh.grid(column=3, row=0, padx=20, pady=5, ipadx=20, ipady=3, sticky="e")
+        btn_container.pack(side="top", fill="both")
+
+        # ================ List 1 ================ 
+        container = self.CreateFormalList()
+        container.pack(padx=20, pady=5, fill="both", expand=True)
+    
+    def get_window(self):
+        return self.main_container
+
+    def CreateFormalList(self):
+        container = tk.Frame(self.main_container, bg="#fff")
+        canv1 = tk.Canvas(container, bg="#fff")
+
+        lbl_frame = tk.Frame(canv1, bg="#fff")
+        lbl_frame.columnconfigure(0, weight=1)
+        lbl_frame.columnconfigure(1, weight=1)
+        lbl_frame.columnconfigure(2, weight=1)
+        lbl_frame.columnconfigure(3, weight=1)
+        lbl_frame.columnconfigure(4, weight=1)
+        lbl_frame.rowconfigure(0, weight=1)
+        lbl = tk.Label(lbl_frame, text="Violation ID", bg="#fff").grid(column=0, row=0, sticky="news")
+        lbl2 = tk.Label(lbl_frame, text="Violation Type", bg="#fff").grid(column=1, row=0, sticky="news")
+        lbl3 = tk.Label(lbl_frame, text="Area", bg="#fff").grid(column=2, row=0, sticky="news")
+        lbl4 = tk.Label(lbl_frame, text="Date", bg="#fff").grid(column=3, row=0, sticky="news")
+        lbl5 = tk.Label(lbl_frame, text="Recording Frame", bg="#fff").grid(column=4, row=0, sticky="news")
+        lbl_frame.pack(ipadx=0, ipady=0, side="top", fill="x")
+
+        lb = tk.Listbox(canv1, width=150, bd=0, relief="solid")
+        lb.pack(side="left", fill="both", expand=True)
+        scroll = tk.Scrollbar(canv1)
+        scroll.pack(side="right", fill="y")
+
+        for i in range(20):
+            lb.insert(tk.END, i)
+        
+        lb.config(yscrollcommand=scroll.set)
+        scroll.config(command=lb.yview)
+        canv1.pack(fill="both", expand=True)
+
+        return container
--- window1.py
+++ window1.py
@@ -3,6 +3,7 @@ from PIL import Image,ImageTk
 from VideoCapture import *
 from tkinter import Toplevel
 from NotificationWindow import *
+from HistoryWindow import *
 import cv2
 
 class Window1():
@@ -29,6 +30,7 @@ class Window1():
         self.display_frame_large = tk.Frame(self.container_frame)
         self.ui_frame = tk.Frame(self.container_frame)
         self.notification_frame = NotificationWindow(self.container_frame).get_window()
+        self.history_frame = HistoryWindow(self.container_frame).get_window()
 
         self.container_frame.columnconfigure(0, weight=2)
         self.container_frame.rowconfigure(0, weight=1)
@@ -151,22 +153,18 @@ class Window1():
     def ShowNotification(self):
         print("Notification Clicked!")
 
-        # self.NotificationWindow = Toplevel(self.root)
-        # self.NotificationWindow.geometry("{}x{}+{}+{}".format(1000, 600, self.pos_x, self.pos_y))
-        # self.NotificationWindow.title("Notifications window")
-
         self.container_frame.update()
 
         if self.display_frame.winfo_manager():
             self.display_frame.grid_forget()
         elif self.display_frame_large.winfo_manager():
             self.display_frame_large.grid_forget()
+        elif self.history_frame.winfo_manager():
+            self.history_frame.grid_forget()
 
         if not self.notification_frame.winfo_manager():
-            print("test1")
             self.notification_frame.grid(column=0, row=0, padx=10, pady=10, sticky="news")
         else:
-            print("test2")
             self.notification_frame.grid_forget()
             self.display_frame.grid(column=0, row=0, padx=40, pady=20, sticky="nw")
 
@@ -174,51 +172,20 @@ class Window1():
     def ShowHistory(self):
         print("History Clicked!")
 
-        self.HistoryWindow = Toplevel(self.root)
-        self.HistoryWindow.geometry("{}x{}+{}+{}".format(1200, 600, self.pos_x, self.pos_y))
-        self.HistoryWindow.title("History window")
+        self.container_frame.update()
 
-        container = tk.Frame(self.HistoryWindow)
+        if self.display_frame.winfo_manager():
+            self.display_frame.grid_forget()
+        elif self.display_frame_large.winfo_manager():
+            self.display_frame_large.grid_forget()
+        elif self.notification_frame.winfo_manager():
+            self.notification_frame.grid_forget()
 
-        canvas = tk.Canvas(container)
-        frame = tk.Frame(canvas)
-        scroll = tk.Scrollbar(self.HistoryWindow, orient="vertical", command=canvas.yview)
-        canvas.configure(yscrollcommand=scroll.set)
-
-        pill1 = self.CreateHistoryPill(frame, title="Recording 1", width=275, height=40, side="left")
-        pill2 = self.CreateHistoryPill(frame, title="Recording 2", width=275, height=40, side="left")
-        pill3 = self.CreateHistoryPill(frame, title="Recording 3", width=275, height=40, side="left")
-        pill4 = self.CreateHistoryPill(frame, title="Recording 4", width=275, height=40, side="left")
-        pill5 = self.CreateHistoryPill(frame, title="Recording 5", width=275, height=40, side="left")
-        pill6 = self.CreateHistoryPill(frame, title="Recording 6", width=275, height=40, side="left")
-        pill7 = self.CreateHistoryPill(frame, title="Recording 7", width=275, height=40, side="left")
-        pill8 = self.CreateHistoryPill(frame, title="Recording 8", width=275, height=40, side="left")
-        pill9 = self.CreateHistoryPill(frame, title="Recording 9", width=275, height=40, side="left")
-        pill10 = self.CreateHistoryPill(frame, title="Recording 10", width=275, height=40, side="left")
-        pill11 = self.CreateHistoryPill(frame, title="Recording 11", width=275, height=40, side="left")
-        pill12 = self.CreateHistoryPill(frame, title="Recording 12", width=275, height=40, side="left")
-        pill13 = self.CreateHistoryPill(frame, title="Recording 13", width=275, height=40, side="left")
-
-        pill1.pack(padx=5, pady=5)
-        pill2.pack(padx=5, pady=5)
-        pill3.pack(padx=5, pady=5)
-        pill4.pack(padx=5, pady=5)
-        pill5.pack(padx=5, pady=5)
-        pill6.pack(padx=5, pady=5)
-        pill7.pack(padx=5, pady=5)
-        pill8.pack(padx=5, pady=5)
-        pill9.pack(padx=5, pady=5)
-        pill10.pack(padx=5, pady=5)
-        pill11.pack(padx=5, pady=5)
-        pill12.pack(padx=5, pady=5)
-        pill13.pack(padx=5, pady=5)
-
-        scroll.pack(side="right", fill="y")
-        canvas.pack(side="left")
-        canvas.create_window((0,0), window=frame, anchor="nw")
-        frame.bind("<Configure>", lambda event : canvas.configure(scrollregion=canvas.bbox("all"), width = 300, height=300))
-
-        container.pack(fill="both", expand=True)
+        if not self.history_frame.winfo_manager():
+            self.history_frame.grid(column=0, row=0, padx=10, pady=10, sticky="news")
+        else:
+            self.history_frame.grid_forget()
+            self.display_frame.grid(column=0, row=0, padx=40, pady=20, sticky="nw")
 
     def ShowMenu(self):
         print("Menu Clicked!")
