6c9b41f2237b5578f4c2ef977b73e022e46b42be
Aloysius Butac <aloysiusbutac@gmail.com>
2023-02-24 14:44:47 +0800

    Minor changes

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ class App(tk.Tk):
 
         self.title('Tkinter MVC Demo')
 
-        win1 = win.Window2(self)
-        # win1.SetStream1(win.MyVideoCapture(0))
-        # win1.SetStream2(win.MyVideoCapture("sample.mkv"))
-        # win1.SetStream3(win.MyVideoCapture("1.gif"))
-        # win1.SetStream4(win.MyVideoCapture("2.gif"))
+        win1 = win.Window1(self)
+        win1.SetStream1(win.MyVideoCapture(0))
+        win1.SetStream2(win.MyVideoCapture("sample.mkv"))
+        win1.SetStream3(win.MyVideoCapture("1.gif"))
+        win1.SetStream4(win.MyVideoCapture("2.gif"))
         win1.show()
 
 if __name__ == '__main__':
     app = App()
-    app.mainloop()
+    # app.mainloop()
